c53414d6bf1c9ea9c4c3104e74ab3ef247d2b2f7
Sachin-75 <sachin.kumavat93@gmail.com>
2024-06-05 14:47:18 +0530

    Chnages done

--- controllers/product_cont.js
+++ controllers/product_cont.js
@@ -1,22 +1,32 @@
 const Product = require("../model/product_model");
 
 const getProductCont = async(req, res) =>{
-    const {company, name} = req.query;
+    const {company, name, feature, sort} = req.query;
     const comQuery = {};
     if(company){
         comQuery.company = company;
         console.log(comQuery);
     }
+    if(feature){
+        comQuery.feature = feature;
+    }
     if(name){
-        comQuery.name = name;
+        comQuery.name = {$regex: name, $options: "i"};
         console.log(comQuery);
     }
-    const apiData = await Product.find(comQuery);
+    let resultApi = Product.find(comQuery);
+
+    if(sort){
+        let sortIssueFix = sort.replace(","," ");
+        resultApi = resultApi.sort(sortIssueFix);
+    }
+
+    const apiData = await resultApi;
     res.status(201).json({ apiData })
 }
 
 const getProductContCheck = async(req, res) =>{
-    const apiData = await Product.find(req.query);
+    const apiData = await Product.find(req.query).select("name");
     console.log(req.query);
     res.status(201).json({ apiData })
 }
